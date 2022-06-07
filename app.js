c33a9eedf8dea4b0164fb0911031ccd4d9467ea2
MatiViglianco <105290418+MatiViglianco@users.noreply.github.com>
2022-06-07 00:45:07 -0300

    finish
    por alguna razon al optimizar el bootstrap no me guarda la parte visual de las listas

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
 let button = document.getElementsByClassName("btn btn-danger");
-const container = document.getElementById("container");
+let personNum = [];
+let total = 0;
+let totalpay = document.getElementById("totalPay");
+let totaldiv = document.getElementById("totalDiv");
 
-function printValues() {
-  const h3 = document.createElement("h3");
-  const value = getValues(csv);
-  h3.innerText = value;
-  container.append(h3);
+function validateForm(text, number) {
+  let flag = false;
+  if (text === "" || number === "") {
+    alert("por favor llenar los espacios en blanco");
+  } else {
+    flag = true;
+  }
+  return flag;
 }
 
-function wiev(event) {
-  let text = document.getElementById("text");
-  let numb = document.getElementById("numb");
+function totalAndDiv(total, div) {
+  totalpay.textContent = `TOTAL A PAGAR:$ ${total}`;
+  totaldiv.textContent = `A CADA UNO LE TOCA PAGAR: ${div.toFixed(2)}`;
+}
 
-  console.log(text.value);
-  console.log(numb.value);
+function printValues(text, numb) {
+  const container = document.querySelector("ul");
+  const li = document.createElement("li");
+  li.textContent = text + "  $" + numb;
+  li.classList.add("list-group-item");
+  container.appendChild(li);
+}
 
-  document.getElementById("text").value = "";
-  document.getElementById("numb").value = "";
+function validatorNaN(text) {
+  let flag = false;
+  let ns = text.split("");
+  for (const i of ns) {
+    if (isNaN(Number(i))) {
+      flag = true;
+      break;
+    }
+  }
+  if (flag === false) {
+    alert("El nombre solo debe contener letras.");
+  }
+  return flag;
+}
+
+function wiew(event) {
+  let text = document.getElementById("text").value;
+  let numb = document.getElementById("numb").value;
+  let noWhite = validateForm(text, numb);
+
+  console.log(total);
+  if (noWhite === true && validatorNaN(text)) {
+    document.getElementById("text").value = "";
+    document.getElementById("numb").value = "";
+    if (personNum.indexOf(text) === -1) {
+      personNum.push(text);
+    }
+    total += Number(numb);
+    let div = total / personNum.length;
+    totalAndDiv(total, div);
+    printValues(text, numb);
+  }
 }
